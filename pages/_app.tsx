488f2c863aa5842b7ed2f829d637e6f299659e4f
junki <junki646@gmail.com>
2023-03-20 00:43:50 +0900

    chore : recoil setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import theme from '@/styles/theme'
 import { GlobalStyle } from '@/styles/global-style'
+import { RecoilRoot } from 'recoil'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </RecoilRoot>
   )
 }
 
