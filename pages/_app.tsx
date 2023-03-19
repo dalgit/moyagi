621c7e85dd0f636b4966025dadda182dbaa138d3
junki <junki646@gmail.com>
2023-03-20 01:00:43 +0900

    chore : react-query, axios setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,20 @@ import { ThemeProvider } from 'styled-components'
 import theme from '@/styles/theme'
 import { GlobalStyle } from '@/styles/global-style'
 import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const queryClient = new QueryClient()
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </RecoilRoot>
   )
 }
