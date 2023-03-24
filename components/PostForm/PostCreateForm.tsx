eeb1c28ac5db709e1eabf899caa2b0fac279d154
junki <junki646@gmail.com>
2023-03-24 23:42:53 +0900

    feat : connecting ref to react-quill using forwardedRef

--- components/PostForm/PostCreateForm.tsx
+++ components/PostForm/PostCreateForm.tsx
@@ -1,10 +1,27 @@
 import 'react-quill/dist/quill.snow.css'
 import axios from 'axios'
 import dynamic from 'next/dynamic'
+import { useRef, RefObject } from 'react'
+import ReactQuill, { ReactQuillProps } from 'react-quill'
 import styled from 'styled-components'
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+
+interface CustomQuillProps extends ReactQuillProps {
+  forwardedRef: RefObject<ReactQuill>
+}
+
+const CustomReactQuill = dynamic(
+  async () => {
+    const { default: ReactQuill } = await import('react-quill')
+    const CustomQuill = ({ forwardedRef, ...props }: CustomQuillProps) => (
+      <ReactQuill ref={forwardedRef} {...props} />
+    )
+    return CustomQuill
+  },
+  { ssr: false },
+)
 
 const PostCreateForm = () => {
+  const quillRef = useRef<ReactQuill>(null)
   const imageHandler = () => {
     const input = document.createElement('input')
     input.setAttribute('type', 'file')
@@ -13,15 +30,15 @@ const PostCreateForm = () => {
     input.click()
 
     input.onchange = async () => {
+      if (!input.files) return
       const [file] = input.files
       const fileName = file.name
+      const editorSelection = quillRef.current?.getEditorSelection()
 
-      await axios
-        .post('/api/awsS3/getS3SignedUrl', { fileName })
-        .then((res) => {
-          const url = res.data
-          axios.put(url, file)
-        })
+      await axios.post('/api/awsS3/getSignedUrl', { fileName }).then((res) => {
+        const url = res.data
+        axios.put(url, file)
+      })
     }
   }
 
@@ -57,16 +74,21 @@ const PostCreateForm = () => {
       handlers: { image: imageHandler },
     },
   }
+
   return (
-    <TMP>
-      <ReactQuill modules={modules} theme="snow" />
-    </TMP>
+    <QuillRayout>
+      <CustomReactQuill
+        forwardedRef={quillRef}
+        modules={modules}
+        theme="snow"
+      />
+    </QuillRayout>
   )
 }
 
 export default PostCreateForm
 
-const TMP = styled.div`
+const QuillRayout = styled.div`
   width: 600px;
   height: 600px;
 `
