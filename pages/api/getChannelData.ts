0e55360483e8c832e7e29f1696cc5e0c2a3617ff
junki <junki646@gmail.com>
2023-03-30 03:57:30 +0900

    feat : add manager and members data to channel data

--- pages/api/getChannelData.ts
+++ pages/api/getChannelData.ts
@@ -1,15 +1,47 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { connectToDatabase } from '@/utils/db/db'
-
 const getChannelData = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const db = await connectToDatabase()
     const channelsCollection = db.collection('channels')
     const { channelAddress } = req.query
 
-    const channel = await channelsCollection.findOne({
-      address: channelAddress,
-    })
+    const channel = await channelsCollection
+      .aggregate([
+        {
+          $match: {
+            address: channelAddress,
+          },
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'members',
+            foreignField: '_id',
+            as: 'members',
+          },
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'manager',
+            foreignField: '_id',
+            as: 'manager',
+          },
+        },
+        {
+          $project: {
+            _id: true,
+            name: true,
+            address: true,
+            description: true,
+            publicStatus: true,
+            manager: { _id: true, name: true },
+            members: { _id: true, name: true },
+          },
+        },
+      ])
+      .next()
 
     res.status(200).json({ channel })
   } catch (error) {
