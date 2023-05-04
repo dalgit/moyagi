8dbd2b117ff687aa6c55f683ac0a200ea159c20f
junki <junki646@gmail.com>
2023-05-04 22:25:35 +0900

    refactor : profile page use tab and new component

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,112 +1,89 @@
-import moment from 'moment'
+import { QueryClient, dehydrate } from '@tanstack/react-query'
 import { GetServerSideProps } from 'next/types'
-import { useState } from 'react'
-import { useRecoilValue } from 'recoil'
-import { usePatchJoinRequestStatus } from '@/hooks/mutations/usePatchJoinRequestsStatus'
-import { useGetChannelJoinRequests } from '@/hooks/queries/useGetChannelJoinRequests'
-import { useGetJoinnedChannels } from '@/hooks/queries/useGetJoinnedChannels'
-import { useGetMyJoinRequests } from '@/hooks/queries/useGetMyJoinRequests'
-import { useGetMyPosts } from '@/hooks/queries/useGetMyPosts'
-import { userSelector } from '@/recoil/user'
+import styled from 'styled-components'
+import {
+  SubscribedChannels,
+  ManagedChannelList,
+} from '@/components/Channel/ChannelList'
+import { Tab } from '@/components/common/Ui/Tab'
+import { MyPostList } from '@/components/Post/PostList'
+import { MyRegistrationList } from '@/components/Registration/RegistrationList'
+import UserInformation from '@/components/User/UserInformation'
 import createServerInstance from '@/utils/axios/server'
 
-const UserProfilePage = ({ userInfo }: any) => {
-  const [clickedChannelId, setClickedChannelId] = useState<string>('')
-  const user = useRecoilValue(userSelector)
-
-  const { data: posts } = useGetMyPosts()
-  const { data: joinRequests } = useGetMyJoinRequests()
-  const { data: joinnedChannels } = useGetJoinnedChannels()
-  const { data: channelJoinRequests } = useGetChannelJoinRequests(
-    clickedChannelId,
-    { enabled: !!clickedChannelId },
+const UserProfilePage = () => {
+  const MyInfoTabPair = () => (
+    <Tab.Pair>
+      <Tab.Title>내정보</Tab.Title>
+      <Tab.Content>
+        <UserInformation />
+      </Tab.Content>
+    </Tab.Pair>
   )
 
-  const managedChannels = joinnedChannels?.filter(
-    (channel) => channel.manager === user?._id,
+  const MyChannelTabPair = () => (
+    <Tab.Pair>
+      <Tab.Title>채널</Tab.Title>
+      <Tab.Content>
+        <DividedChannelTab />
+      </Tab.Content>
+    </Tab.Pair>
   )
 
-  const subscribedChannels = joinnedChannels?.filter(
-    (channel) => channel.manager !== user?._id,
-  )
+  const DividedChannelTab = () => {
+    const ManagedChannelsTabPair = () => (
+      <Tab.Pair>
+        <Tab.Title>운영</Tab.Title>
+        <Tab.Content>
+          <ManagedChannelList />
+        </Tab.Content>
+      </Tab.Pair>
+    )
 
-  const handleChannelClick = async (channelId: string) => {
-    setClickedChannelId(channelId)
+    const SubscribedChannelsTabPair = () => (
+      <Tab.Pair>
+        <Tab.Title>가입 </Tab.Title>
+        <Tab.Content>
+          <SubscribedChannels />
+        </Tab.Content>
+      </Tab.Pair>
+    )
+
+    return (
+      <Tab>
+        <ManagedChannelsTabPair />
+        <SubscribedChannelsTabPair />
+      </Tab>
+    )
   }
 
-  const handleJoinRequestStatus = async (
-    requestId: string,
-    channelId: string,
-    isApproved: boolean,
-  ) => {
-    const status = isApproved ? 'approve' : 'reject'
-    patchJoinRequestStatusMutate({ channelId, requestId, status })
-  }
+  const MyRegistrationTabPair = () => (
+    <Tab.Pair>
+      <Tab.Title>가입신청</Tab.Title>
+      <Tab.Content>
+        <MyRegistrationList />
+      </Tab.Content>
+    </Tab.Pair>
+  )
 
-  const { mutate: patchJoinRequestStatusMutate } = usePatchJoinRequestStatus()
+  const MyPostsTabPair = () => (
+    <Tab.Pair>
+      <Tab.Title>내 글 보기</Tab.Title>
+      <Tab.Content>
+        <MyPostList />
+      </Tab.Content>
+    </Tab.Pair>
+  )
 
   return (
-    <div>
-      <h3>내정보</h3>
-      <div>{user?.name}</div>
-      <h3>운영중인 채널</h3>
-      {managedChannels?.map((channel) => {
-        return (
-          <>
-            <p
-              key={channel._id}
-              onClick={() => handleChannelClick(channel._id)}
-            >
-              {channel?.name}
-            </p>
-            {channelJoinRequests?.map((request) => {
-              return (
-                <div key={request._id}>
-                  닉네임: {request.requestor.name}
-                  메시지: {request.message}
-                  <button
-                    onClick={() =>
-                      handleJoinRequestStatus(request._id, channel._id, true)
-                    }
-                  >
-                    승인
-                  </button>
-                  <button
-                    onClick={() =>
-                      handleJoinRequestStatus(request._id, channel._id, false)
-                    }
-                  >
-                    거절
-                  </button>
-                  ss
-                </div>
-              )
-            })}
-          </>
-        )
-      })}
-      <h3>가입한 채널</h3>
-      {subscribedChannels?.map((channel) => {
-        return <p key={channel._id}>{channel?.name}</p>
-      })}
-      <div>
-        <h3>가입심사중인 채널</h3>
-        {joinRequests?.map((request) => {
-          return (
-            <div key={request._id}>
-              <p>채널이름 : {request.channel.name}</p>
-              <p>채널설명 : {request.channel.description}</p>
-              <p>상태: {request.status}</p>
-              <p>요청일시: {moment(request.time).format('YYYY-MM-DD HH:mm')}</p>
-            </div>
-          )
-        })}
-      </div>
-      <h3>내 글 보기</h3>
-      {posts?.map((post) => (
-        <div>{post.content}</div>
-      ))}
-    </div>
+    <ProfilePageLayout>
+      <Tab>
+        <MyInfoTabPair />
+        <MyChannelTabPair />
+        <MyRegistrationTabPair />
+        <MyPostsTabPair />
+      </Tab>
+    </ProfilePageLayout>
   )
 }
 
@@ -114,12 +91,14 @@ export default UserProfilePage
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const server = createServerInstance(context)
+  const queryClient = new QueryClient()
 
   try {
-    const userInfo = await server.get('/users/me').then((res) => res.data)
-
+    await queryClient.fetchQuery(['users', 'me'], () =>
+      server.get('/users/me').then((res) => res.data),
+    )
     return {
-      props: { userInfo },
+      props: { dehydratedProps: dehydrate(queryClient) },
     }
   } catch {
     return {
@@ -130,3 +109,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
 }
+
+const ProfilePageLayout = styled.div`
+  padding: 20px 150px 0 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
