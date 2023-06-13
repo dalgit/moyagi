c226060b6aa9dce0c1147810930eac575cc30d0d
junki <junki646@gmail.com>
2023-06-14 06:44:31 +0900

    feat : update useForm hook to include form validation

--- hooks/common/useForm.ts
+++ hooks/common/useForm.ts
@@ -1,14 +1,37 @@
 import { useState } from 'react'
 
-const useForm = <T>(initialValue: T) => {
-  const [form, setForm] = useState(initialValue)
+const useForm = <
+  T extends Record<string, string>,
+  V extends Record<string, (value: string, form?: T) => boolean>,
+>(
+  initialForm: T,
+  validationFunctions: V,
+) => {
+  const [form, setForm] = useState(initialForm)
+
+  const [isValid, setIsValid] = useState<Record<keyof V, boolean>>(() => {
+    const isValidKeys = Object.keys(validationFunctions)
+
+    return isValidKeys.reduce(
+      (acc, cur) => ({ ...acc, [cur]: false }),
+      {} as Record<keyof V, boolean>,
+    )
+  })
 
   const updateForm = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setForm({ ...form, [name]: value })
+
+    if (validationFunctions[name]) {
+      setIsValid({ ...isValid, [name]: validationFunctions[name](value, form) })
+    }
   }
 
-  return { form, updateForm }
+  const isAllValid = () => {
+    return Object.values(isValid).every((item) => item)
+  }
+
+  return { form, updateForm, isValid, isAllValid }
 }
 
 export default useForm
